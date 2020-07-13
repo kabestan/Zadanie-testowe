2a78f8d4f674d9e4162264ae3c63aa8d70d798a2
Michał Dyrka <dyrka.michal@gmail.com>
2020-07-13 14:47:45 +0200

    Created database table.

---
+++ RCP - Zadanie Testowe/Database/RCPlogs.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[RCPlogs]
+(
+	[RecordId] INT NOT NULL PRIMARY KEY,
+	[Timestamp] SMALLDATETIME NOT NULL, 
+    [WorkerId] INT NOT NULL, 
+    [ActionType] TINYINT NOT NULL, 
+    [LoggerType] TINYINT NOT NULL
+)
