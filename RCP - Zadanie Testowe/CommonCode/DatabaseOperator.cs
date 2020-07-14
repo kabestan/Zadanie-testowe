cf3ad849121c82c2e04e22a6d0e5fb7793c59aea
Michał Dyrka <dyrka.michal@gmail.com>
2020-07-14 10:24:42 +0200

    Added project with common class for database operations.

---
+++ RCP - Zadanie Testowe/CommonCode/DatabaseOperator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace CommonCode
+{
+    public class DatabaseOperator
+    {
+        private const string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        private SqlConnection connection;
+
+        public DatabaseOperator()
+        {
+            connection = new SqlConnection(connectionString);
+        }
+
+        public bool Open()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.Message);
+                return false;
+            }
+
+        }
+
+        public void Close()
+        {
+            connection.Close();
+        }
+    }
+}
--- RCP - Zadanie Testowe/DesktopApp/App.xaml.cs
+++ RCP - Zadanie Testowe/DesktopApp/App.xaml.cs
@@ -9,6 +9,7 @@ using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
+using CommonCode;
 
 namespace DesktopApp
 {
