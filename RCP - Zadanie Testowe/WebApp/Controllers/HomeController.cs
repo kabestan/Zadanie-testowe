307216e90121df039ecb569296ff1de390f0854b
Michał Dyrka <dyrka.michal@gmail.com>
2020-07-20 20:31:02 +0200

    Created new view from template for Log.

--- RCP - Zadanie Testowe/WebApp/Controllers/HomeController.cs
+++ RCP - Zadanie Testowe/WebApp/Controllers/HomeController.cs
@@ -23,6 +23,11 @@ namespace WebApp.Controllers
             return View();
         }
 
+        public IActionResult Log()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
